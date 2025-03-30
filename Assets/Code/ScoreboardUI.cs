1d0abd423332339d62802709863f63d3e11990e5
lwonsang <l.wonsang@wustl.edu>
2025-03-29 20:06:47 -0500

    created a scoreboard UI that shows round number or score

---
+++ Assets/Code/ScoreboardUI.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class ScoreboardUI : MonoBehaviour
+{
+    public TextMeshProUGUI roundNumberText;
+    public TextMeshProUGUI winnerText;
+    public Button nextButton;
+    public Canvas scoreboardCanvas;
+
+    private int currentRound = 1;
+    private bool gameOver = false;
+
+    void Start()
+    {
+        // Set initial UI state
+        scoreboardCanvas.gameObject.SetActive(false);
+        nextButton.onClick.AddListener(OnNextButtonClick);
+        
+        // Display the round number at the start
+        UpdateRoundUI();
+    }
+
+    void UpdateRoundUI()
+    {
+        scoreboardCanvas.gameObject.SetActive(true);
+        roundNumberText.text = "Round " + currentRound;
+    }
+
+    public void ShowFinalScore(string winner, float bluePercentage, float redPercentage)
+    {
+        // Hide the round number text
+        roundNumberText.gameObject.SetActive(false);
+
+        // Show final results
+        winnerText.gameObject.SetActive(true);
+        winnerText.text = $"{winner} Wins!\n\nBlue: {bluePercentage:F2}%\nRed: {redPercentage:F2}%";
+
+        gameOver = true; // Mark the game as over
+    }
+
+    void OnNextButtonClick()
+    {
+        if (gameOver)
+        {
+            SceneManager.LoadScene("Main Menu"); // Load main menu if game is over
+        }
+        else
+        {
+            currentRound++;
+            UpdateRoundUI();
+        }
+    }
+}
